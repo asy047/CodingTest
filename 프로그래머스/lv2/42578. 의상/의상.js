30e73a8070bd74ad45a675e789b261a41710d71a
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-24 02:26:04 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -0,0 +1,19 @@
+function solution(clothes) {
+    var answer = 1;
+    let hashTable = {}; // 값을 받을 Hash Table 생성 
+
+    clothes.forEach((element) => { 
+      let key = element[1]; // 최초 key 값 생성
+        
+      if (hashTable[key] === undefined) { // 최초 key 값에서 value를 찾았을 때 undefined가 나오면
+          hashTable[key] = [element[0]];  // 새로 key: value 생성
+      } else {
+          hashTable[key].push(element[0]);// 있으면 key 값에 value 추가
+      }
+    });
+
+    for(const [key, value] of Object.entries(hashTable)) { // Object.entries -> key, value로 제어 가능
+        answer *= (value.length + 1);                      
+    }
+    return answer - 1; // 조건부 확률 => (2 + 1) * (1 + 1) - 1
+}
