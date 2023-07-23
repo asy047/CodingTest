0f1400c4206f0500e05d62680a9b990e9ae2c96a
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-23 17:22:00 +0900

    [unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -4,10 +4,18 @@
 using namespace std;
 
 string solution(string str1, string str2) {
+    // 내 풀이
+    // string answer = "";
+    // for(int i = 0; i < str1.length(); i++){
+    //     answer += str1[i];
+    //     answer += str2[i];
+    // 
+    
     string answer = "";
-    for(int i = 0; i < str1.length(); i++){
-        answer += str1[i];
-        answer += str2[i];
+    for(int i=0; i<str1.length(); i++)
+    {
+        answer.push_back(str1[i]); // push_back의 존재를 알다..
+        answer.push_back(str2[i]);
     }
     return answer;
 }
