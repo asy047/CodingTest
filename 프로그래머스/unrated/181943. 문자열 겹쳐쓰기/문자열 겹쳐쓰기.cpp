8aa6328aecc1af4e60c0a93c1e555d25f6d8d10f
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-23 16:58:56 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 string solution(string my_string, string overwrite_string, int s) {
+    //str.raplace(n, k, str2) str의 n번쨰 인덱스부터 k까지 str2로 대체
     my_string.replace(s, overwrite_string.length(), overwrite_string);
     string answer = my_string;
     return answer;
