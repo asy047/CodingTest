e73ec88fc4033e760ce26e163b7a501fe3448f58
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-27 12:38:12 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,12 @@
+function solution(code) {
+    let mode = 0;
+    let ret = "";
+    code.split('').forEach((v, idx) => {
+        if(mode === 0) {
+            v === '1' ? mode = 1 : idx % 2 == 0 ? ret += v : 0;
+        } else if (mode === 1) {
+            v === '1' ? mode = 0 : idx % 2 == 0 ? 0 : ret += v;;
+        }
+    })
+    return ret === "" ? "EMPTY" : ret;
+}
