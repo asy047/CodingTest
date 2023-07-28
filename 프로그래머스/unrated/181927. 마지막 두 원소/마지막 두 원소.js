cce91853610eea3aa960a03fe9deb651de35e2e1
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-28 13:52:54 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,6 +1,10 @@
 function solution(num_list) {
-     num_list[num_list.length - 1] > num_list[num_list.length - 2] ? 
-         num_list.push(num_list[num_list.length - 1] - num_list[num_list.length - 2]) :
-         num_list.push(num_list[num_list.length - 1] * 2)
-    return num_list
+    //  num_list[num_list.length - 1] > num_list[num_list.length - 2] ? 
+    //      num_list.push(num_list[num_list.length - 1] - num_list[num_list.length - 2]) :
+    //      num_list.push(num_list[num_list.length - 1] * 2)
+    // return num_list
+    
+    const [a, b] = [...num_list].reverse(); // 인덱스 기준으로 reverse 한 값 앞에 두개를 a, b에 받음
+    return [...num_list, a > b ? (a-b) : a*2]; // 비교해서 기존 num_list와 a b 비교한 값을 넣음
+
 }
