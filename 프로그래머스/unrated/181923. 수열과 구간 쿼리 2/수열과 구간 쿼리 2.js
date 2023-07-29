a6a81d6fcd078c914878d17ef052c2e25ae2a12d
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-29 16:33:44 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.77 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,6 @@
+function solution(arr, queries) {
+   return queries.map(([s, e, k]) => {
+       let num = Math.min(...arr.slice(s,e+1).filter(el => el > k));
+       return num === Infinity ? -1 : num;
+    })
+}
