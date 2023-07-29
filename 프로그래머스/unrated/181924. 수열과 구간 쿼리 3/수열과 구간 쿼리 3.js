f940657518d1e56b60d32d5f4593749ac168c3e4
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-29 16:13:50 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,6 @@
 function solution(arr, queries) {
-    queries.map((v) => {
-        sw(arr, v[0], v[1])
+   queries.forEach(( [a, b] ) => {
+        [arr[a], arr[b]] = [arr[b], arr[a]];
     })
     return arr;
-}
-function sw(arr, index1, index2) {
-    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
-    return arr[index1];
 }
