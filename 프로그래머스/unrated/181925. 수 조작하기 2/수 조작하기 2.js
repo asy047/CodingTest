2669ff8b6992de0c85b130df3563dffd5a676264
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-07-29 15:43:09 +0900

    [unrated] Title: 수 조작하기 2, Time: 8.67 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,14 +1,22 @@
 function solution(numLog) {
-    let result = ""
-    numLog.forEach((v, idx) => {
-        let num = Math.abs(numLog[idx+1] - v);
-        if(num){
-        num === 1 ? 
-            numLog[idx + 1] > v ? 
-            result += "w" : result += "s"
-        : numLog[idx + 1] > v ?
-            result += "d" : result += "a"
-        }
-    })
-    return result;
+    // let result = ""
+    // numLog.forEach((v, idx) => {
+    //     let num = Math.abs(numLog[idx+1] - v);
+    //     if(num){
+    //     num === 1 ? 
+    //         numLog[idx + 1] > v ? 
+    //         result += "w" : result += "s"
+    //     : numLog[idx + 1] > v ?
+    //         result += "d" : result += "a"
+    //     }
+    // })
+    // return result;
+    
+    const convert = {
+        '1': 'w', '-1': 's', '10': 'd', '-10': 'a'
+    };
+
+    return numLog.slice(1).map((v, i) => {
+        return convert[v - numLog[i]]
+    }).join('')
 }
