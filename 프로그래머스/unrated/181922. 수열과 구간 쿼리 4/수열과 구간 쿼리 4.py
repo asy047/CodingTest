03aedf746f72c398995abc0cdef5b7b65c52163d
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-08-11 15:56:23 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 4.37 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    result = arr.copy()  # 결과 배열을 초기 배열로 복사
+    
+    for query in queries:
+        s, e, k = query
+        for i in range(s, e + 1):
+            if i % k == 0:
+                result[i] += 1
+    
+    return result
