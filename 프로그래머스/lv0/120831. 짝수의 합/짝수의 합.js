29ab6f5d91b473deb1ec413da50aec1f54584351
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-08-25 10:33:28 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    let answer = 0;
-    let i = 0;
-    while(i <= n) {
-        i % 2 === 0 ? answer += i : i;
-        i++;
-    }
-    return answer;
+    // let answer = 0;
+    // let i = 0;
+    // while(i <= n) {
+    //     i % 2 === 0 ? answer += i : i;
+    //     i++;
+    // }
+    // return answer;
+    var half = Math.floor(n/2);
+    return half*(half+1);
 }
