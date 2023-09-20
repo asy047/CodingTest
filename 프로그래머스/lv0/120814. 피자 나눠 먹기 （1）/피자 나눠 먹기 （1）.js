40da059a6b17db729bd4f00dac36100c4bb1ed45
An Seo-young <67266378+asy047@users.noreply.github.com>
2023-09-20 11:42:55 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,3 +1,5 @@
 function solution(n) {
-    return n > 7 ? n % 7 === 0 ? parseInt(n/7) : parseInt(n/7) + 1 : 1;
+    //return n > 7 ? n % 7 === 0 ? parseInt(n/7) : parseInt(n/7) + 1 : 1;
+        return Math.ceil(n / 7)
+
 }
